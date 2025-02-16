3c0e205abb8faa6cf837ce19f8b1c682c7fdd792
Nithish <nithishbharadwaj.akula@gmail.com>
2025-02-16 14:31:59 -0500

    commit

--- src/main/scala/Demo/if_else.scala
+++ src/main/scala/Demo/if_else.scala
@@ -1,3 +1,15 @@
 package Demo
 
-object if_else
+object if_else {
+  def main(args: Array[String]): Unit = {
+    val x =20;
+    if (x ==20){
+      print("x == 20, nuv super raa")
+
+    }else {
+      print("x != 20")
+    }
+  }
+}
+
+
