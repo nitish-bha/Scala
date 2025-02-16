b394369f28227ae820df060a5506027ebf7caf6b
Nithish <nithishbharadwaj.akula@gmail.com>
2025-02-16 18:55:30 -0500

    commit

---
+++ src/main/scala/Demo/Functions.scala
@@ -0,0 +1,10 @@
+package Demo
+
+object Functions {
+  def add(x : Int, y : Int) : Int = {
+    return x+y;
+  }
+  def main (args: Array[String]): Unit = {
+    print(add(30928, 782632445))
+  }
+}
