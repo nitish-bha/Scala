0d09ee6b0f852fe8e8dd200e51e1ba1401b1255d
Nithish <nithishbharadwaj.akula@gmail.com>
2025-02-16 15:16:10 -0500

    commit

--- src/main/scala/Demo/While_Loop.scala
+++ src/main/scala/Demo/While_Loop.scala
@@ -1,6 +1,6 @@
 package demo
 
-object While_Loop {
+object WhileLoop {
   def main(args: Array[String]): Unit = {
     var x = 0
     while (x < 10) {
@@ -8,10 +8,10 @@ object While_Loop {
       x += 1
     }
 
-    var y = 0;
-    do {
+    var y = 0
+    while (y < 1) {
       println(s"y = $y")
       y += 1
-    } while (y < 0)
+    }
   }
 }
